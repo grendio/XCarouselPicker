d8805119d94a7ebdf5154cb706e2d829b537cbc3
Alex Stefan <alex.steefan@gmail.com>
2019-01-21 23:43:20 +0200

    Automatically select middle item from CollectionView

--- XCarousel.Touch/MainViewController.cs
+++ XCarousel.Touch/MainViewController.cs
@@ -1,19 +1,33 @@
-﻿using UIKit;
+﻿using CoreGraphics;
+using Foundation;
+using UIKit;
 
 namespace XCarousel.Touch
 {
     public partial class MainViewController : UIViewController
     {
+        private NSIndexPath previouslyVisibleItem;
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
             customCollectionView.RegisterNibForCell(CustomCollectionViewCell.Nib, CustomCollectionViewCell.Key);
             customCollectionView.CollectionViewLayout = new CustomFlowLayout();
-            customCollectionView.Delegate = new CustomCollectionViewDelegate();
             customCollectionView.DataSource = new CustomCollectionViewDataSource();
-            customCollectionView.DecelerationRate = UIScrollView.DecelerationRateFast;
-            customCollectionView.AllowsMultipleSelection = true;
+            customCollectionView.DecelerationEnded += CustomCollectionView_DecelerationEnded;
+        }
+
+        void CustomCollectionView_DecelerationEnded(object sender, System.EventArgs e)
+        {
+            var visibleRect = new CGRect(customCollectionView.ContentOffset, customCollectionView.Bounds.Size);
+            var visiblePoint = new CGPoint(visibleRect.GetMidX(), visibleRect.GetMidY());
+            var visibleIndexPath = customCollectionView.IndexPathForItemAtPoint(visiblePoint);
+
+            if (previouslyVisibleItem != null && customCollectionView.CellForItem(previouslyVisibleItem) != null)
+                customCollectionView.CellForItem(previouslyVisibleItem).Frame = new CGRect(customCollectionView.CellForItem(previouslyVisibleItem).Frame.Location, new CGSize(100.0, 150.0));
+            customCollectionView.CellForItem(visibleIndexPath).Frame = new CGRect(customCollectionView.CellForItem(visibleIndexPath).Frame.Location, new CGSize(100.0, 170.0));
+            previouslyVisibleItem = visibleIndexPath;
         }
     }
 }
