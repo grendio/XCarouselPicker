da6c34c7006685b0cd4c3f9e32d1d318cb19080e
Alexandru Stefan <alex.steefan@gmail.com>
2019-01-24 23:07:08 +0100

    Added assests and method to scale cell

--- XCarousel.Touch/CustomFlowLayout.cs
+++ XCarousel.Touch/CustomFlowLayout.cs
@@ -6,59 +6,10 @@ namespace XCarousel.Touch
 {
     public class CustomFlowLayout : UICollectionViewFlowLayout
     {
-        public CustomFlowLayout()
+        public CustomFlowLayout(nfloat width, nfloat height)
         {
-            this.ItemSize = new CGSize(100, 150);
-            this.ScrollDirection = UICollectionViewScrollDirection.Horizontal;
-        }
-
-        CGPoint mostRecentOffset = new CGPoint();
-
-        public override CGPoint TargetContentOffset(CGPoint proposedContentOffset, CGPoint scrollingVelocity)
-        {
-            if (scrollingVelocity.X == 0)
-            {
-                return mostRecentOffset;
-            }
-
-            var cv = this.CollectionView;
-            if (cv != null)
-            {
-                var cvBounds = this.CollectionView.Bounds;
-                var halfWidth = cvBounds.Size.Width * 0.5f;
-                var attributesForVisibleCells = this.LayoutAttributesForElementsInRect(cvBounds);
-                if (attributesForVisibleCells != null)
-                {
-                    if (proposedContentOffset.X == -(cv.ContentInset.Left))
-                    {
-                        return proposedContentOffset;
-                    }
-
-                    UICollectionViewLayoutAttributes candidateAttributes = null;
-                    foreach (var attributes in attributesForVisibleCells)
-                    {
-                        if (attributes.RepresentedElementCategory != UICollectionElementCategory.Cell)
-                        {
-                            continue;
-                        }
-
-                        if ((attributes.Center.X == 0) || (attributes.Center.X > (cv.ContentOffset.X + halfWidth) && scrollingVelocity.X < 0))
-                        {
-                            continue;
-                        }
-                        candidateAttributes = attributes;
-                    }
-
-                    if (candidateAttributes == null)
-                    {
-                        return mostRecentOffset;
-                    }
-
-                    return mostRecentOffset = new CGPoint(candidateAttributes.Center.X - halfWidth, proposedContentOffset.Y);
-                }
-            }
-
-            return mostRecentOffset = base.TargetContentOffset(proposedContentOffset, scrollingVelocity);
+            ItemSize = new CGSize(width, height);
+            ScrollDirection = UICollectionViewScrollDirection.Horizontal;
         }
     }
 }
--- XCarousel.Touch/MainViewController.cs
+++ XCarousel.Touch/MainViewController.cs
@@ -1,4 +1,6 @@
-﻿using UIKit;
+﻿using System;
+using CoreGraphics;
+using UIKit;
 
 namespace XCarousel.Touch
 {
@@ -9,9 +11,28 @@ namespace XCarousel.Touch
             base.ViewDidLoad();
 
             customCollectionView.RegisterNibForCell(CustomCollectionViewCell.Nib, CustomCollectionViewCell.Key);
-            customCollectionView.CollectionViewLayout = new CustomFlowLayout();
+            customCollectionView.CollectionViewLayout = new CustomFlowLayout(50, 100);
+            customCollectionView.Frame = new CGRect(customCollectionView.Frame.Location, new CGSize(View.Frame.Size.Width, 100));
             customCollectionView.DataSource = new CustomCollectionViewDataSource();
-            customCollectionView.Delegate = new CustomCollectionViewDelegate();
+            customCollectionView.Scrolled += CustomCollectionView_Scrolled;
+        }
+
+        void CustomCollectionView_Scrolled(object sender, EventArgs e)
+        {
+            var cells = customCollectionView.VisibleCells;
+
+            var visibleRect = new CGRect(customCollectionView.ContentOffset, customCollectionView.Bounds.Size);
+            var visiblePoint = new CGPoint(visibleRect.GetMidX(), visibleRect.GetMidY());
+            var visibleIndexPath = customCollectionView.IndexPathForItemAtPoint(visiblePoint);
+
+            if (visibleIndexPath != null)
+            {
+                if (customCollectionView.CellForItem(visibleIndexPath) != null)
+                {
+                    var middleCell = customCollectionView.CellForItem(visibleIndexPath);
+                    middleCell.Transform = CGAffineTransform.MakeScale(1, new nfloat(1.5));
+                }
+            }
         }
     }
 }
