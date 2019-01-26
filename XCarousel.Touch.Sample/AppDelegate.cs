495ebf6e707c7aba2450f1ace5b5b4343f3c7188
Alexandru Stefan <alex.steefan@gmail.com>
2019-01-27 00:31:19 +0200

    Add sample project for iOS part

---
+++ XCarousel.Touch.Sample/AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using Foundation;
+using UIKit;
+
+namespace XCarousel.Touch.Sample
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            Window.RootViewController = new ViewController(); // First screen
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
---
+++ XCarousel.Touch.Sample/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XCarousel.Touch.Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController() : base("ViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
